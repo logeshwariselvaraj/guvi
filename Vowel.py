f35154b2dd9572536052983ae6c7700c11d1f710
logeshwariselvaraj <50898782+logeshwariselvaraj@users.noreply.github.com>
2019-05-28 12:16:33 +0530

    Create Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,5 @@
+logu=input("")
+if(logu=="a" or logu=="e" or logu=="i" or logu=="o" or logu=="u" or logu=="A" or logu=="E" or logu=="I" or logu=="O" or logu=="U" ):
+  print("Vowel")
+else:
+  print("Consonant")
